0c684162a9d9cdbf050ea55775b85ba6b1c3d808
zomsantech-devops <116977095+zomsantech-devops@users.noreply.github.com>
2024-03-12 16:01:30 +0700

    test

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.all("*", function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
-});
+}); 
 
 app.use("/api/v1/auth", authRoute);
 
